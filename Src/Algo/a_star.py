c2bbdfbce6d607f3f810198c80e9496f56250623
ntritran999 <ntritran999@gmail.com>
2025-06-27 11:26:55 +0700

    Add A* algo

---
+++ Src/Algo/a_star.py
@@ -0,0 +1,40 @@
+from queue import PriorityQueue
+from .state import *
+from itertools import count
+
+def heuristic(car):
+    return abs(car.coord[1] - 4)
+
+def a_star(start):
+    frontier = PriorityQueue()
+    unique = count()
+    dist = {}
+    result = None
+
+    frontier.put((heuristic(start.cars[0]), next(unique), start))
+    while not frontier.empty():
+        cost, _, current = frontier.get()
+        if current.cars[0].coord[1] + current.cars[0].length == 6:
+            result = current
+            break
+
+        current_tuple = current.to_tuple()
+        if current_tuple in dist and dist[current_tuple] < cost: # Skip longer path
+            continue
+
+        for state in current.next_states():
+            new_cost = heuristic(state.cars[0]) + state.cost
+            state_tuple = state.to_tuple()
+            if state_tuple not in dist or new_cost < dist[state_tuple]:
+                dist[state_tuple] = new_cost
+                frontier.put((new_cost, next(unique), state))
+
+    if not result:
+        return [start]
+    
+    path = []
+    while result != start:
+        path.append(result)
+        result = result.parent
+    path.reverse()
+    return path
