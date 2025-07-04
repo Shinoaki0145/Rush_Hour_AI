5073ff2639e0f59fc669321e3c95cdd74b99f3f8
ntritran999 <ntritran999@gmail.com>
2025-07-04 11:12:03 +0700

    Add template for new create_3d_text in ui.py

--- Src/UI/ui.py
+++ Src/UI/ui.py
@@ -22,3 +22,27 @@ def create_3d_text(screen, text, size, x, y):
     # Vẽ chữ chính
     main_surface = font.render(text, True, WHITE)
     screen.blit(main_surface, (x, y))
+
+# from math import sqrt
+
+# def create_3d_text(screen, text, size, ratio_x, ratio_y, x, y):
+#     """Tạo hiệu ứng chữ 3D với viền"""
+    
+#     font = pygame.font.Font(FONT, int(size * sqrt(ratio_x ** 2 + ratio_y ** 2)))
+    
+#     # Vẽ outline (viền)
+#     outline_offsets = [
+#         (-2, -2), (-2, -1), (-2, 0), (-2, 1), (-2, 2),
+#         (-1, -2), (-1, 2),
+#         (0, -2), (0, 2),
+#         (1, -2), (1, 2),
+#         (2, -2), (2, -1), (2, 0), (2, 1), (2, 2)
+#     ]
+    
+#     for dx, dy in outline_offsets:
+#         outline_surface = font.render(text, True, BLUE_LIGHT)
+#         screen.blit(outline_surface, (x + dx, y + dy))
+    
+#     # Vẽ chữ chính
+#     main_surface = font.render(text, True, WHITE)
+#     screen.blit(main_surface, (x, y))
